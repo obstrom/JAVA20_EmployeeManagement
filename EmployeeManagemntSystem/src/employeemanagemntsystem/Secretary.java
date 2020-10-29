172fc11aea0077d93ff9a1315c4f0364de5611d5
Oscar Bergström <oscar@obstrom.com>
2020-10-29 16:10:29 +0100

    Add bonus salary support

--- EmployeeManagemntSystem/src/employeemanagemntsystem/Secretary.java
+++ EmployeeManagemntSystem/src/employeemanagemntsystem/Secretary.java
@@ -1,6 +1,8 @@
 package employeemanagemntsystem;
 
 public class Secretary extends Employee {
+    private static final double BONUS = 0.03;
+
     Secretary(int ID, String firstName, String lastName, int salary, Gender gender, String dateOfBirth, Department department) {
         super(ID, firstName, lastName, salary, gender, dateOfBirth, department);
     }
@@ -22,13 +24,18 @@ public class Secretary extends Employee {
     }
 
     @Override
-    void updateSalary(int newSalary) {
-        super.setSalary(newSalary);
+    void updateBaseSalary(int newBaseSalary) {
+        super.setBaseSalary(newBaseSalary);
     }
 
     @Override
     void updateGender(Gender newGender) {
         super.setGender(newGender);
+    }
+
+    @Override
+    double getBonus() {
+        return this.getBaseSalary() * BONUS;
     }
 
 }
