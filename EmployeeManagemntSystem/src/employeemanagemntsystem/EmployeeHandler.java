2d793a377c01e213646c1d5981f841485a383cb5
Oscar Bergström <oscar@obstrom.com>
2020-10-29 14:56:55 +0100

    Made class content static

--- EmployeeManagemntSystem/src/employeemanagemntsystem/EmployeeHandler.java
+++ EmployeeManagemntSystem/src/employeemanagemntsystem/EmployeeHandler.java
@@ -3,20 +3,24 @@ package employeemanagemntsystem;
 import java.util.ArrayList;
 
 public class EmployeeHandler {
-    private ArrayList<Employee> allEmployees = new ArrayList<Employee>();
+    static private final ArrayList<Employee> ALL_EMPLOYEES = new ArrayList<Employee>();
 
     public void createNewEmployee(EmployeeCategory employeeCategory, int ID, String firstName, String lastName, int salary, Gender gender, String dateOfBirth, Department department) {
         switch (employeeCategory) {
-            case MANAGER -> allEmployees.add(new Manager(ID, firstName, lastName, salary, gender, dateOfBirth, department));
-            case TECHNICIAN -> allEmployees.add(new Technician(ID, firstName, lastName, salary, gender, dateOfBirth, department));
-            case SECRETARY -> allEmployees.add(new Secretary(ID, firstName, lastName, salary, gender, dateOfBirth, department));
-            case PROGRAMMER -> allEmployees.add(new Programmer(ID, firstName, lastName, salary, gender, dateOfBirth, department));
+            case MANAGER -> ALL_EMPLOYEES.add(new Manager(ID, firstName, lastName, salary, gender, dateOfBirth, department));
+            case TECHNICIAN -> ALL_EMPLOYEES.add(new Technician(ID, firstName, lastName, salary, gender, dateOfBirth, department));
+            case SECRETARY -> ALL_EMPLOYEES.add(new Secretary(ID, firstName, lastName, salary, gender, dateOfBirth, department));
+            case PROGRAMMER -> ALL_EMPLOYEES.add(new Programmer(ID, firstName, lastName, salary, gender, dateOfBirth, department));
         }
     }
 
-    public Employee findEmployee(int employeeID) {
+    public static ArrayList<Employee> getAllEmployees() {
+        return ALL_EMPLOYEES;
+    }
+
+    public static Employee findEmployee(int employeeID) {
         Employee returnObject = null;
-        for (Employee employee: allEmployees) {
+        for (Employee employee: ALL_EMPLOYEES) {
             if (employee.getID() == employeeID) {
                 returnObject = employee;
             }
@@ -24,9 +28,9 @@ public class EmployeeHandler {
         return returnObject;
     }
 
-    public ArrayList<Employee> findEmployee(Department department) {
+    public static ArrayList<Employee> findEmployee(Department department) {
         ArrayList<Employee> returnList = new ArrayList<Employee>();
-        for (Employee employee: allEmployees) {
+        for (Employee employee: ALL_EMPLOYEES) {
             if (employee.getDepartment() == department) {
                 returnList.add(employee);
             }
@@ -34,9 +38,9 @@ public class EmployeeHandler {
         return returnList;
     }
 
-    public ArrayList<Employee> findEmployee(String name) {
+    public static ArrayList<Employee> findEmployee(String name) {
         ArrayList<Employee> returnList = new ArrayList<Employee>();
-        for (Employee employee: allEmployees) {
+        for (Employee employee: ALL_EMPLOYEES) {
             if (employee.getFullName().toLowerCase().contains(name.toLowerCase())) {
                 returnList.add(employee);
             }
@@ -44,9 +48,9 @@ public class EmployeeHandler {
         return returnList;
     }
 
-    public ArrayList<Employee> findEmployee(Gender gender) {
+    public static ArrayList<Employee> findEmployee(Gender gender) {
         ArrayList<Employee> returnList = new ArrayList<Employee>();
-        for (Employee employee: allEmployees) {
+        for (Employee employee: ALL_EMPLOYEES) {
             if (employee.getGender() == gender) {
                 returnList.add(employee);
             }
@@ -56,7 +60,7 @@ public class EmployeeHandler {
 
     public void removeEmployee(Employee employee) {
         try {
-            allEmployees.remove(employee);
+            ALL_EMPLOYEES.remove(employee);
             System.out.println(employee.getFullName() + " has been removed.");
         } catch (Exception e) {
             System.out.println("No employee to remove.");
