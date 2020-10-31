2a1f3d31df0592c4c6ac9514de1f878e1f65013a
Linda-86 <73649487+Linda-86@users.noreply.github.com>
2020-11-01 00:08:00 +0100

    uppdaterat 1/11
    Jag la till meddelandet som ska visas när user väljer exit i både menyn Employeemenu och statisticmenu

--- EmployeeManagementSystem/src/employeeManagementSystem/employeeManagementSystem/MenuEmployeeSystem.java
+++ EmployeeManagementSystem/src/employeeManagementSystem/employeeManagementSystem/MenuEmployeeSystem.java
@@ -79,6 +79,7 @@ public class MenuEmployeeSystem {
                 printAllEmployees();
 
             } else if (userInput.equals("0") || userInput.contains("exit") || userInput.contains("quit")) {
+                System.out.println("Thank you for using our EmployeeManagementSystem");
                 break; // Gå tillbaka till start menu
             } else {
                 System.out.println("Unknown input. Try again!" + newLine);
@@ -365,6 +366,7 @@ public class MenuEmployeeSystem {
                     System.out.println(employee.getFullName() + " department changed to " + employee.getDepartment());
                     break;
                 } else if (userInput.equals("0") || userInput.contains("return") || userInput.contains("exit")) {
+                    System.out.println("Thank you for using our EmployeeManagementSystem");
                     break updateMenuLoop;
                 } else {
                     System.out.println("Unknown input. Try again!" + newLine);
